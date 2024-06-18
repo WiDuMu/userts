38bfa099acb010658419f831830a6cc4332c0741
WiDuMu <97642890+WiDuMu@users.noreply.github.com>
2024-06-17 22:58:23 -0500

    beginning

---
+++ readme.md
@@ -0,0 +1,5 @@
+# UserTS
+This is a project attempting to allow userscripts to be created using modern syntax.
+
+## Naming
+UserTS is named after the conventional extension of a userscript, `.user.ts`.
---
+++ src/user.ts
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        New script 
+// @namespace   Violentmonkey Scripts
+// @match       *://example.org/*
+// @grant       none
+// @version     1.0
+// @author      -
+// ==/UserScript==
+
+
+console.log(`hello world!`);
